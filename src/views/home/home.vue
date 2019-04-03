b7d2c8f37b46580308fe02aab553578fbe5769bf
TheLastDark <yinghuo68@163.com>
2019-04-03 17:56:38 +0800

    add  element

---
+++ src/views/home/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    home
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
---
+++ src/views/login/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    这里是登陆注册
+  </div>
+</template>
+<script>
+export default {
+  name: 'Login',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
